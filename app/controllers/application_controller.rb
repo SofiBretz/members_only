7123ba0ffcde5a85749a0942794c897dd3e267e5
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-27 18:38:20 -0300

    added private post params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-  
-  def current_user=(user)
-  @current_user = user
-  end
-
-  def current_user
-    remember_token = User.digest(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
-  end
-
-  def signed_in?
-    !current_user.nil?
-  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,8 @@
 class PostsController < ApplicationController
   before_action :signed_in_user, only: [:new, :create]
 
-  # before filter/action
-  def signed_in_user
-    unless signed_in?
-      redirect_to signin_url
-    end
+  def index
+   @posts = Post.all
   end
 
   def new
@@ -17,5 +14,17 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     @post.save
     redirect_to root_path
+  end
+
+  def signed_in_user
+    unless signed_in?
+      redirect_to signin_url
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
